035dd3a70174086d453a0b14551c4dce4c92d0d7
Jacob Weidokal <jacob.t.weidokal@nasa.gov>
2022-11-15 11:49:51 -0500

    updating tests

--- MSTestProject/UserServiceTest.cs
+++ MSTestProject/UserServiceTest.cs
@@ -22,6 +22,8 @@ namespace MSTestProject
             GC.Collect();
         }
 
+        #region Basic Tests
+
         [TestMethod]
         public void Should_Not_SignIn_If_No_Credentials()
         {
@@ -55,6 +57,10 @@ namespace MSTestProject
             Assert.AreEqual(result, false);
         }
 
+        #endregion
+
+        #region Testing Exceptions
+
         [TestMethod]
         [ExpectedException(typeof(ArgumentException), "Parameter username may not be null or blank")]
         public void Null_Username_In_Constructor()
@@ -68,5 +74,45 @@ namespace MSTestProject
         {
             User logonInfo = new User(0, "Test", "test@test.com", null);
         }
+
+        #endregion
+
+        #region Data Tests (run test multiple times with different sets of data)
+
+        [DataTestMethod]
+        [DataRow("INVALID_NAME", "INVALID_PASS", false)]
+        [DataRow("TrainingUser", "Training@1", true)]
+        public void Perform_Multiple_SignIn_Checks(string userName, string password, bool expected)
+        {
+            // Arrange
+            User user = new User(0, userName, null, password);
+            // Act
+            var result = _userService.SignIn(user);
+            // Assert
+            Assert.AreEqual(result, expected);
+        }
+
+        [DataTestMethod]
+        [DynamicData(nameof(UserData), DynamicDataSourceType.Method)]
+        public void Perform_Multiple_SignIn_Checks_Dynamic_Data(string userName, string password, bool expected)
+        {
+            // Arrange
+            User user = new User(0, userName, null, password);
+            // Act
+            var result = _userService.SignIn(user);
+            // Assert
+            Assert.AreEqual(result, expected);
+        }
+
+        private static IEnumerable<object[]> UserData()
+        {
+            return new[]
+            {
+                new object[] { "INVALID_NAME", "INVALID_PASS", false},
+                new object[] { "TrainingUser", "Training@1", true }
+            };
+        }
+
+        #endregion
     }
 }
