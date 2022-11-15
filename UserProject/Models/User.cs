eff812a07f08d2da4d15231dec530764be2da7e9
Jacob Weidokal <jacob.t.weidokal@nasa.gov>
2022-11-15 11:31:07 -0500

    add ctors and exceptions to user model

--- UserProject/Models/User.cs
+++ UserProject/Models/User.cs
@@ -1,10 +1,47 @@
-﻿namespace UserProject.Models
+﻿using System.Numerics;
+using System.Reflection.Metadata;
+
+namespace UserProject.Models
 {
     public class User
     {
+        public User()
+        {
+
+        }
+
+        public User(int id, string? userName, string? email, string? password)
+        {
+            Id = id;
+            UserName = userName;
+            Email = email;
+            Password = password;
+        }
         public int Id { get; set; }
-        public string? UserName { get; set; }
+        private string? userName;
+        public string? UserName
+        {
+            get { return userName; }
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                    throw (new ArgumentException("Parameter username may not be null or blank", "UserName"));
+                userName = value;
+                    
+            }
+        }   
         public string? Email { get; set; }
-        public string? Password { get; set; }
+        private string? password;
+        public string? Password
+        {
+            get { return password; }
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                    throw (new ArgumentException("Parameter username may not be null or blank", "Password"));
+                password = value;
+
+            }
+        }
     }
 }
